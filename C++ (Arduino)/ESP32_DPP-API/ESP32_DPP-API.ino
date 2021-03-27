9a3a8ecff51609f9c0b7367eec0d09d3af0e969a
MeCoolGJK <45541051+MeCoolGJK@users.noreply.github.com>
2021-03-27 21:39:23 +0100

    Update ESP32_DPP-API.ino
    Added:
    - Serial.printing values
    - Time parsing to single strings

--- C++ (Arduino)/ESP32_DPP-API/ESP32_DPP-API.ino
+++ C++ (Arduino)/ESP32_DPP-API/ESP32_DPP-API.ino
@@ -15,17 +15,19 @@ const String ContentType = "application/json; charset=utf-8";
 unsigned long lastTime = 0;
 unsigned long timerDelay = 30000;
 
-const int limit = 4;
+const int limit = 8;
 
 const char* line[limit] = {};
-const char* time_predicted[limit] = {};
 const char* final_stop[limit] = {};
+String hour_predicted[limit] = {};
+String minute_predicted[limit] = {};
+String second_predicted[limit] = {};
 
 
 void setup()
 {
 
-    Serial.begin(115200);
+    Serial.begin(38400);
 
     WiFi.begin(SSID, PASS);
     Serial.println("Connecting");
@@ -49,7 +51,7 @@ void loop()
             
       HTTPClient http;
 
-      String serverConditions = "?minutesBefore=10&minutesAfter=60&names=Sl%C3%A1nsk%C3%A1&preferredTimezone=Europe%2FPrague&limit=" + String(limit);
+      String serverConditions = "?minutesAfter=60&names=Sl%C3%A1nsk%C3%A1&preferredTimezone=Europe%2FPrague&limit=" + String(limit);
       String serverPath = serverName + serverConditions;
               
       http.begin(serverPath.c_str());
@@ -62,7 +64,7 @@ void loop()
       String payload = http.getString();
       Serial.println(payload);
 
-      DynamicJsonDocument doc(3072);
+      DynamicJsonDocument doc(8000);
       
       DeserializationError error = deserializeJson(doc, payload);
       
@@ -78,40 +80,51 @@ void loop()
 
         JsonObject root = doc[i];
 
-        const char* root_arrival_timestamp_predicted = root["arrival_timestamp"]["predicted"];
-        const char* root_arrival_timestamp_scheduled = root["arrival_timestamp"]["scheduled"];
+        String root_arrival_timestamp_predicted = root["arrival_timestamp"]["predicted"];
+        String root_arrival_timestamp_scheduled = root["arrival_timestamp"]["scheduled"];
 
         JsonObject root_delay = root["delay"];
-        bool root_delay_is_available = root_delay["is_available"]; // true
-        int root_delay_minutes = root_delay["minutes"]; // 2
-        int root_delay_seconds = root_delay["seconds"]; // 134
+        bool root_delay_is_available = root_delay["is_available"];
+        int root_delay_minutes = root_delay["minutes"];
+        int root_delay_seconds = root_delay["seconds"];
 
         const char* root_departure_timestamp_predicted = root["departure_timestamp"]["predicted"];
         const char* root_departure_timestamp_scheduled = root["departure_timestamp"]["scheduled"];
 
-        const char* root_route_short_name = root["route"]["short_name"]; // "225"
-        const char* root_route_type = root["route"]["type"]; // "3"
+        const char* root_route_short_name = root["route"]["short_name"];
+        const char* root_route_type = root["route"]["type"];
 
         JsonObject root_stop = root["stop"];
-        const char* root_stop_id = root_stop["id"]; // "U236Z4P"#include <b64.h>
-        const char* root_stop_name = root_stop["name"]; // "Slánská"
-        const char* root_stop_platform_code = root_stop["platform_code"]; // "D"
-        int root_stop_wheelchair_boarding = root_stop["wheelchair_boarding"]; // 0
+        const char* root_stop_id = root_stop["id"];
+        const char* root_stop_name = root_stop["name"];
+        const char* root_stop_platform_code = root_stop["platform_code"];
+        int root_stop_wheelchair_boarding = root_stop["wheelchair_boarding"];
 
         JsonObject root_trip = root["trip"];
-        const char* root_trip_headsign = root_trip["headsign"]; // "Velká Ohrada"
-        const char* root_trip_id = root_trip["id"]; // "225_277_210101"
-        bool root_trip_is_canceled = root_trip["is_canceled"]; // false
-        int root_trip_is_wheelchair_accessible = root_trip["is_wheelchair_accessible"]; // 1
+        const char* root_trip_headsign = root_trip["headsign"];
+        const char* root_trip_id = root_trip["id"];
+        bool root_trip_is_canceled = root_trip["is_canceled"];
+        int root_trip_is_wheelchair_accessible = root_trip["is_wheelchair_accessible"];
 
-        Serial.print(root_route_short_name);
-        Serial.print(" ");
-        Serial.print(root_trip_headsign);
-        Serial.print(" ");
-        Serial.println(root_arrival_timestamp_predicted);
-        
-
+        line[i] = root_route_short_name;
+        final_stop[i] = root_trip_headsign;
+        hour_predicted[i] = root_arrival_timestamp_predicted.substring(11, 13); // Fixed positions
+        minute_predicted[i] = root_arrival_timestamp_predicted.substring(14, 16); // Fixed positions
+        second_predicted[i] = root_arrival_timestamp_predicted.substring(17, 19); // Fixed positions
       }
+      for(int i = 0; i < limit; i++)
+       {
+         
+        Serial.print(line[i]);
+        Serial.print(" - ");
+        Serial.print(final_stop[i]);
+        Serial.print(" - ");
+        Serial.print(hour_predicted[i]);
+        Serial.print(":");
+        Serial.print(minute_predicted[i]);
+        Serial.print(":");
+        Serial.println(second_predicted[i]);
+      }        
 
       lastTime = millis();
       http.end();
