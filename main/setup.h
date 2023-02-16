5fc074ae77fb3ab0583699ed369d9ebc3d942e65
MeCoolGJK <45541051+MeCoolGJK@users.noreply.github.com>
2023-02-16 21:33:11 +0100

    Update setup.h

--- main/setup.h
+++ main/setup.h
@@ -26,12 +26,12 @@
 // Pokud chcete použít dvě odlišné zastávky (např. vlakovou stanici a tramvajovou stanici opodál),
 // oddělte jednotlivé zastávky pomocí "&"" a před dalším názvem zastávky použijte znovu "names=".
 // Příklad: "names=N%C3%A1dra%C5%BE%C3%AD%20Podbaba%0A&names=Praha-Podbaba"
-#define ZASTAVKA_CELA ""
+#define ZASTAVKA_CELA "names=And%C4%9Bl"
 
 // ID zastávky, ID získáte podle testovacího výstupu v GOLEMIO, vložte mezi uvozovky za "ids=".
 // Pokud chcete použít dva odlišné sloupky (jako například sloupek autobusu a tramvaje),
 // oddělte jednotlivé ID pomocí & a před dalším ID použijte znovu "ids=". Příklad: "ids=U236Z1P&ids=U236Z4P"
-#define ZASTAVKOVY_SLOUPEK "ids=U236Z1P&ids=U236Z4P"
+#define ZASTAVKOVY_SLOUPEK ""
 
 
 #endif /* SETUP_H */
