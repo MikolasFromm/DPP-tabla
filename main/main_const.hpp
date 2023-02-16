efcf19b42bc5e6970f30bb9137569bddc003693c
Mikoláš Fromm <mikolas.fromm@gmail.com>
2023-02-16 21:34:29 +0100

    Create main_const.hpp

---
+++ main/main_const.hpp
@@ -0,0 +1,9 @@
+#ifndef main_const_hpp_
+#define main_const_hpp_
+
+#define DOWNLOAD_LIMIT 5
+#define TIMEZONE "preferredTimezone=Etc%2FGMT"
+#define CONTENT_TYPE "application/json; charset=utf-8";
+
+
+#endif
