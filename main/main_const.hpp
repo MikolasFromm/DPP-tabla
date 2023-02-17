5aac749b3799aea24382c0d63da440d490b63439
Mikoláš Fromm <mikolas.fromm@gmail.com>
2023-02-17 18:28:15 +0100

    Update main_const.hpp

--- main/main_const.hpp
+++ main/main_const.hpp
@@ -2,8 +2,9 @@
 #define main_const_hpp_
 
 #define DOWNLOAD_LIMIT 5
+#define ADDITIONAL_TIME_TO_STOP -3
 #define TIMEZONE "preferredTimezone=Etc%2FGMT"
-#define CONTENT_TYPE "application/json; charset=utf-8";
+#define CONTENT_TYPE "application/json; charset=utf-8"
 
 
 #endif
