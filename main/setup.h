06e36d4a2f640901de9c2f61e79a381ff6688fa0
Mikoláš Fromm <mikolas.fromm@gmail.com>
2023-02-17 18:28:22 +0100

    Update setup.h

--- main/setup.h
+++ main/setup.h
@@ -20,18 +20,17 @@
 // Zde vložte vaši zastávku - !! vždy však pouze jeden ze dvou níže uvedených údajů. Druhý nechejte prázdný. !!
 // Pole _ZASTAVKA_ pro všechny směry jedné zastávky.
 // Pole _ZASTAVKOVY-SLOPEK_ pro jeden konkrétní směr.
-// Více na <https://github.com/MeCoolGJK/DPP-tabla>
 
 // Název stanice musí být v URL UTF-8 encoded formátu, vložte mezi uvozovky za "names=".
 // Pokud chcete použít dvě odlišné zastávky (např. vlakovou stanici a tramvajovou stanici opodál),
 // oddělte jednotlivé zastávky pomocí "&"" a před dalším názvem zastávky použijte znovu "names=".
-// Příklad: "names=N%C3%A1dra%C5%BE%C3%AD%20Podbaba%0A&names=Praha-Podbaba"
-#define ZASTAVKA_CELA "names=And%C4%9Bl"
+// Příklad: "names=N%C3%A1dra%C5%BE%C3%AD%20Podbaba%0A&names=Praha-Podbaba" nebo "names=And%C4%9Bl"
+#define ZASTAVKA_CELA ""
 
 // ID zastávky, ID získáte podle testovacího výstupu v GOLEMIO, vložte mezi uvozovky za "ids=".
 // Pokud chcete použít dva odlišné sloupky (jako například sloupek autobusu a tramvaje),
 // oddělte jednotlivé ID pomocí & a před dalším ID použijte znovu "ids=". Příklad: "ids=U236Z1P&ids=U236Z4P"
-#define ZASTAVKOVY_SLOUPEK ""
+#define ZASTAVKOVY_SLOUPEK "ids=U236Z1P&ids=U236Z4P"
 
 
 #endif /* SETUP_H */
