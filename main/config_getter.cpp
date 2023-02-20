89c3dd2accf69068a8ec76d643e9c97a20c6fcaa
Mikoláš Fromm <mikolas.fromm@gmail.com>
2023-02-20 19:34:57 +0100

    Loading consts from .txt file

--- main/config_getter.cpp
+++ main/config_getter.cpp
@@ -1,100 +1,130 @@
 #include "config_getter.hpp"
 
-void config_getter::read_config()
+void config_getter::read_config(TFT_eSPI& tft)
 {
-    enum load_status {nothing, whole_stop, stop_sign, timeout};
-    load_status current_status = load_status::nothing;
-    std::string buffer;
-
-    if (SD.begin(13, 15, 2, 14))
+    enum load_status {waiting, stop, time, nick, ssid, pass};
+    load_status current_status = load_status::waiting;
+    
+    tft.setTextDatum(MC_DATUM);
+    tft.setTextSize(3);
+    while(!SD.begin(13, 15, 2, 14))
     {
+        // IDLE to connect SD card
+        tft.setTextColor(TFT_RED, TFT_BLACK);
+        tft.drawString("INSERT SD CARD", 160, 60, 1);
+    }
+    // success notification
+    tft.fillScreen(TFT_GREEN);
+    tft.setTextColor(TFT_BLACK);
+    tft.drawString("Succesfully", 160, 60, 1);
+    tft.drawString(" loaded SD ", 160, 90, 1);
+    delay(2000);
+    tft.fillScreen(TFT_BLACK);
+    
+    ext::File new_file = SD.open("/config.txt", F_READ);
+    if (new_file)
+    {
+        std::string buffer;
         bool line_comment = false;
-        ext::File new_file = SD.open("/config.txt", FILE_READ);
-        if (new_file)
+        while(new_file.available())
         {
-            while(new_file.available())
+            char current_char = new_file.read();
+            if (std::isspace(current_char) || std::iscntrl(current_char)) // when on space, save the word
             {
-                char current_char = new_file.read();
-                if (std::isspace(current_char) || std::iscntrl(current_char)) // when on space, save the word
+                if (buffer != "" && !line_comment)
                 {
-                    if (buffer != "" && !line_comment)
+                    if (current_status != load_status::waiting)
+                            buffer = buffer.substr(1, buffer.size()-2); // removing  '"' before and after word
+                    switch (current_status)
                     {
-                        if ((buffer == "Sloupky:" || buffer == "Zastavky:") && current_status != load_status::nothing)
-                        {
-                            // ERROR!
-                        }
-                        if (buffer == "TimeOffset:")
-                        {
-                            current_status = load_status::nothing; // fallback jump to ignore the "TimeOffset" word and start reading the number
-                        }
-                        switch (current_status)
-                        {
-                            case load_status::nothing:
-                                if (buffer == "Zastavky:")
-                                {
-                                    current_status = load_status::whole_stop;
-                                }
-                                else if (buffer == "Sloupky:")
-                                {
-                                    current_status = load_status::stop_sign;
-                                }
-                                else if (buffer == "TimeOffset:")
-                                {
-                                    current_status = load_status::timeout;
-                                }
-                                else
-                                {
-                                    // ERROR!
-                                }
-                                break;
-                            case load_status::stop_sign:
-                                this->stop_signs.push_back(buffer);
-                                break;
-                            case load_status::whole_stop:
-                                this->stop_names.push_back(buffer);
-                                break;
-                            case load_status::timeout:
-                                this->time_offset = std::stoi(buffer);
-                                break;
+                        case load_status::waiting:
+                            if (buffer == "Z:")
+                            {
+                                current_status = load_status::stop;
                             }
-                        buffer = "";
-                    }
+                            else if (buffer == "N:")
+                            {
+                                current_status = load_status::nick;
+                            }
+                            else if (buffer == "T:")
+                            {
+                                current_status = load_status::time;
+                            }
+                            else if (buffer == "W:")
+                            {
+                                current_status = load_status::ssid;
+                            }
+                            else if (buffer == "P:")
+                            {
+                                current_status = load_status::pass;
+                            }
+                            else
+                            {
+                                // ERROR! WRONG DATA FORMAT
+                            }
+                            break;
+                        case load_status::stop:
+                        {
+                            // if nickname or time to stop not given, pushing back default
+                            std::size_t count_of_stops = this->stop_names.size();
+                            if (this->walktime_to_stop.size() < count_of_stops)
+                            {
+                                this->walktime_to_stop.push_back(0);
+                            }
+                            if (this->stop_nicknames.size() < count_of_stops)
+                            {
+                                std::string unknown_name = "\"Neznámé jméno\"";
+                                this->stop_nicknames.push_back(unknown_name);
+                            }
+                            // pushing back the new stop
+                            this->stop_names.push_back(buffer);
+                            current_status = load_status::waiting;
+                            break;
+                        }
+                        case load_status::nick:
+                            this->stop_nicknames.push_back(buffer);
+                            current_status = load_status::waiting;
+                            break;
+                        case load_status::time:
+                            this->walktime_to_stop.push_back(std::stoi(buffer));
+                            current_status = load_status::waiting;
+                            break;
+                        case load_status::ssid:
+                            this->ssid = buffer;
+                            current_status = load_status::waiting;
+                            break;
+                        case load_status::pass:
+                            this->wifi_pass = buffer;
+                            current_status = load_status::waiting;
+                            break;
+                        }
+                    buffer = "";
                 }
-                else // otherwise save the letter to buffer
-                {
-                    if (current_char == '/' || current_char == '#') // ignoring line comments
-                        line_comment = true;
-                    if (!line_comment) // saving to buffer only when not in commented line
-                        buffer += current_char;
-                }
-                if (current_char == '\n') // when on new_line char
-                    line_comment = false;
             }
+            else // otherwise save the letter to buffer
+            {
+                if (current_char == '/' || current_char == '#') // ignoring line comments
+                    line_comment = true;
+                if (!line_comment) // saving to buffer only when not in commented line
+                    buffer += current_char;
+            }
+            if (current_char == '\n') // when on new_line char
+                line_comment = false;
         }
-        else
-        {
-            Serial.println("Couldnt open file");
-        }
-        new_file.close();
-        SD.end();
     }
     else
     {
-        Serial.println("COULDNT BEGIN SD");
+        tft.fillScreen(TFT_BLACK);
+        tft.setTextDatum(MC_DATUM);
+        tft.setTextSize(3);
+        tft.setTextColor(TFT_RED, TFT_BLACK);
+        tft.drawString("CONFIG NOT FOUND!", 160, 60, 1);
+        while (true)
+        {
+            // RABBIT HOLE
+        }
     }
-    Serial.println("ZASTAVKY: ");
-    for (size_t i = 0; i < this->stop_names.size(); i++)
-    {
-        Serial.print(this->stop_names[i].c_str());
-        Serial.print(" ; ");
-    }
-    Serial.println("SLOUPKY: ");
-    for (size_t i = 0; i < this->stop_signs.size(); i++)
-    {
-        Serial.print(this->stop_signs[i].c_str());
-        Serial.print(" ; ");
-    }
-    Serial.println();
-    
+    new_file.close();
+    SD.end();
     return;
 }
--- main/config_getter.hpp
+++ main/config_getter.hpp
@@ -3,17 +3,31 @@
 
 #include <mySD.h>
 #include <SPI.h>
+#include <TFT_eSPI.h>
 #include <vector>
 #include <string>
 
 class config_getter
 {
   public:
-  void read_config();
+  void read_config(TFT_eSPI& tft);
+  void increment_stop_index() { this->stop_index = (this->stop_index+stop_names.size()+1)%stop_names.size();}
+  void decrement_stop_index() { this->stop_index = (this->stop_index+stop_names.size()-1)%stop_names.size();}
+  int get_current_stop_index() { return this->stop_index;}
+  std::string& get_current_stop() { return this->stop_names[this->stop_index];}
+  std::string& get_current_stop_nickname() { return this->stop_nicknames[this->stop_index];}
+  int get_current_stop_walktime() { return this->walktime_to_stop[this->stop_index];}
+  std::string& get_ssid() { return this->ssid;}
+  std::string& get_wifi_pass() { return this->wifi_pass;}
+
+
   private:
+  int stop_index = 0;
   std::vector<std::string> stop_names;
-  std::vector<std::string> stop_signs;
-  int time_offset;
+  std::vector<int> walktime_to_stop;
+  std::vector<std::string> stop_nicknames;
+  std::string ssid;
+  std::string wifi_pass;
 };
 
 #endif
