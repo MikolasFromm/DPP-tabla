bb8d547c239cf644d2578ffaabcfa4e6c7c3f0a9
MeCoolGJK <45541051+MeCoolGJK@users.noreply.github.com>
2024-03-03 21:07:41 +0100

    Removed certificate check

--- main/payload_parser.cpp
+++ main/payload_parser.cpp
@@ -23,21 +23,34 @@ int payload_parser::input_data_check(std::string& stop_name, int walktime_to_sto
 
 int payload_parser::deserialize_document()
 {
-  this->start_http_client();
-  Serial.print("Deserialization started: ");
-  Serial.println(this->serverPath.c_str());
-  int request_https_code = this->http.GET();
-  if (request_https_code == 200)
-  {
-    this->doc = SpiRamJsonDocument(1048576);
-    DeserializationError json_err = deserializeJson(this->doc, this->http.getStream());
-    if (json_err)
+  try 
+  { 
+    this->start_http_client();
+    Serial.print("Deserialization started: ");
+    Serial.println(this->serverPath.c_str());
+    int request_https_code = this->http.GET();
+    if (request_https_code == 200)
     {
-      log_e("JSON deserialization failed: %s", json_err.c_str()); // SHOULD BE EXCEPTION
+      this->doc = SpiRamJsonDocument(1048576);
+      String payload = this->http.getString();
+      DeserializationError json_err = deserializeJson(this->doc, payload);
+      if (json_err)
+      {
+        log_e("JSON deserialization failed: %s", json_err.c_str()); // SHOULD BE EXCEPTION
+      }
     }
+    else
+    {
+      Serial.print("status_code: ");
+      Serial.println(request_https_code);
+    }
+    this->end_http_client();
+    return request_https_code;
+  } 
+  catch (const std::exception& e) 
+  { 
+    Serial.println(e.what());
   }
-  this->end_http_client();
-  return request_https_code;
 }
 
 void payload_parser::load_api_from_config(std::string& api)
@@ -47,8 +60,9 @@ void payload_parser::load_api_from_config(std::string& api)
 
 void payload_parser::start_http_client()
 {
-  this->http.useHTTP10(true);
-  this->http.begin(this->serverPath.c_str(), this->cert.c_str());
+//  this->http.useHTTP10(true);
+//  this->http.begin(this->serverPath.c_str(), this->cert.c_str());
+  this->http.begin(this->serverPath.c_str());
   this->http.addHeader("x-access-token", this->myAPI.c_str());
   this->http.addHeader("content-type",  this->ContentType.c_str());
 }
